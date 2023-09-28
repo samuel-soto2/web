ef45f54a3db64b150e194c54e69bcf16db1361ad
samuel-soto2 <1461888@senati.pe>
2023-09-28 16:44:53 -0500

    login

--- includes/login.php
+++ includes/login.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -9,8 +7,6 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	
-
 </head>
 
 <body>
@@ -21,30 +17,3 @@
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <br>
-           
-                   <br>
-                                    <h3 class="text-center">Iniciar Sesión</h3>
-                       <br>
-                            <div class="form-group">
-                                <label for="correo">Usuario:</label><br>
-                                <input type="text" name="nombre" id="nombre" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Contraseña:</label><br>
-                                <input type="password" name="password" id="password" class="form-control" required>
-                                <input type="hidden" name="accion" value="acceso_user">
-                            </div>
-                            <div class="form-group">
-                             <br>
-                    <center>
-                                <input type="submit"class="btn btn-success" value="Ingresar">   
-                                </center>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </form>
-</body>
-</html>
