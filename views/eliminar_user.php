3a1f13113aa244091c47de51a1f32955baba1b3b
samuel-soto2 <1461888@senati.pe>
2023-09-28 17:18:20 -0500

    para confirmar la eliminacion

--- views/eliminar_user.php
+++ views/eliminar_user.php
@@ -20,3 +20,29 @@ error_reporting(0);
     <title>Eliminar Usuarios</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<body>
+    
+    <div class="container mt-5">
+    <div class="row">
+    <div class="col-sm-6 offset-sm-3">
+    <div class="alert alert-danger text-center">
+    <p>¿Desea confirmar la eliminacion del registro?</p>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <form action="../includes/_functions.php" method="POST">
+                <input type="hidden" name="accion" value="eliminar_registro">
+                <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+                <input type="submit" name="" value="Eliminar" class= " btn btn-danger">
+                <a href="user.php" class="btn btn-success">Cancelar</a>
+
+                               
+        </div>
+    </div>
+
+
+
+</body>
+</html>
+
