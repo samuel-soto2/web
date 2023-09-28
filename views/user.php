da412c8666d645e12668db881f05055e9be88eb4
samuel-soto2 <1461888@senati.pe>
2023-09-28 17:10:00 -0500

    restricciones

--- views/user.php
+++ views/user.php
@@ -61,4 +61,57 @@ error_reporting(0);
                         </tr>
                         </thead>
                         <tbody>
+                        <?php
+
+$conexion=mysqli_connect("localhost","root","","r_user");               
+$SQL="SELECT *FROM user ";
+$dato = mysqli_query($conexion, $SQL);
+
+if($dato -> num_rows >0){
+    while($fila=mysqli_fetch_array($dato)){
+    
+?>
+<tr>
+<td><?php echo $fila['nombre']; ?></td>
+<td><?php echo $fila['correo']; ?></td>
+<td><?php echo $fila['password']; ?></td>
+<td><?php echo $fila['telefono']; ?></td>
+<td><?php echo $fila['fecha']; ?></td>
+<td>
+<a class="btn btn-warning" href="editar_user.php?id=<?php echo $fila['id']?> ">
+<i class="fa fa-edit"></i> </a>
+
+  <a class="btn btn-danger" href="eliminar_user.php?id=<?php echo $fila['id']?>">
+<i class="fa fa-trash"></i></a>
+  
+</td>
+</tr>
+
+
+<?php
+}
+}else{
+
+    ?>
+    <tr class="text-center">
+    <td colspan="16">No existen registros</td>
+    </tr>
+
+    
+    <?php
+    
+}
+
+?>
+
+
+	</body>
+  </table>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+<script src="../js/user.js"></script>
+
+
+</html>
+
 
